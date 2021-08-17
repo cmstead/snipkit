300d9f6c34fe1f5d5ecbe7053ca69bfcdf9c4d33
Chris Stead <cmstead@gmail.com>
2021-08-17 16:50:36 -0700

    *** Working on body string cleaning

--- modules/transforms/string-to-body.js
+++ modules/transforms/string-to-body.js
@@ -1,8 +1,17 @@
-function transformToBodyText(textToTransform) {
-    return textToTransform
+function transformTextLine(textToTransform) {
+    const transformedText = textToTransform
         .trim()
-        .replace(/([^\\])\"/ig, "$1\\\"")
-        .replace(/([^\\])\$/ig, "$1\\\$");
+        .replace(/(^|[^\\])\"/ig, "$1\\\"")
+        .replace(/(^|[^\\])\$/ig, "$1\\$");
+
+    return `"${transformedText}"`;
+}
+
+function transformToBodyText(textToTransform) {
+    const transformedTextLines = textToTransform.split('\n')
+        .map((textLine) => transformTextLine(textLine));
+
+    return transformedTextLines.join(',\n');
 }
 
 module.exports = {
--- test/transforms/string-to-body.test.js
+++ test/transforms/string-to-body.test.js
@@ -7,7 +7,25 @@ const { transformToBodyText} = require('../../modules/transforms/string-to-body'
 describe('Transform selected string text to snippet body text', function () {
     it('transforms a single line into body-safe text',  function () {
         const rawText = `      this is a test with "quotes" and a $-sign`;
-        const expectedText = `this is a test with \\"quotes\\" and a \\$-sign`;
+        const expectedText = `"this is a test with \\"quotes\\" and a \\$-sign"`;
+
+        const actualText = transformToBodyText(rawText);
+
+        assert.equal(actualText, expectedText);
+    });
+    it('transforms a single line into body-safe text',  function () {
+        const rawText = [
+            `      this is a test with "quotes" and a $-sign`,
+            '      more text',
+            ' "MORE!"',
+            '            MOARRRR!$$$!'
+        ].join('\n');
+        const expectedText = [
+            `"this is a test with \\"quotes\\" and a \\$-sign"`,
+            '"more text"',
+            '"\\"MORE!\\""',
+            '"MOARRRR!\\$\\$\\$!"'
+        ].join(',\n');
 
         const actualText = transformToBodyText(rawText);
 
