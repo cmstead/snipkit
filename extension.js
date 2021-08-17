9938e53b65df9c9e482ad8a192756ae65b25d002
Chris Stead <cmstead@gmail.com>
2021-08-17 14:47:42 -0700

    e Prepping for new command work

--- extension.js
+++ extension.js
@@ -10,10 +10,6 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "snipkit" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
