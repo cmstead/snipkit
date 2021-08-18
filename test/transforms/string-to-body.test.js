a25e34c684133d865c71ab54c00f58087773ecc7
Chris Stead <cmstead@gmail.com>
2021-08-18 10:28:20 -0700

    t Refactored escaped slashes to named vars

--- test/transforms/string-to-body.test.js
+++ test/transforms/string-to-body.test.js
@@ -2,18 +2,21 @@ require('../utilities/approvals').configure();
 
 const { assert } = require('chai');
 
-const { transformToBodyText} = require('../../modules/transforms/string-to-body');
+const { transformToBodyText } = require('../../modules/transforms/string-to-body');
+
+const slash = "\\";
+const doubleSlash = "\\\\";
 
 describe('Transform selected string text to snippet body text', function () {
-    it('transforms a single line into body-safe text',  function () {
+    it('transforms a single line into body-safe text', function () {
         const rawText = `      this is a test with "quotes" and a $-sign`;
-        const expectedText = `"this is a test with \\"quotes\\" and a \\\\$-sign"`;
+        const expectedText = `"this is a test with ${slash}"quotes${slash}" and a ${doubleSlash}$-sign"`;
 
         const actualText = transformToBodyText(rawText);
 
         assert.equal(actualText, expectedText);
     });
-    it('transforms a single line into body-safe text',  function () {
+    it('transforms a single line into body-safe text', function () {
         const rawText = [
             `      this is a test with "quotes" and a $-sign`,
             '      more text',
@@ -21,10 +24,10 @@ describe('Transform selected string text to snippet body text', function () {
             '            MOARRRR!$$$!'
         ].join('\n');
         const expectedText = [
-            `"this is a test with \\"quotes\\" and a \\\\$-sign"`,
+            `"this is a test with ${slash}"quotes${slash}" and a ${doubleSlash}$-sign"`,
             '"more text"',
-            '"\\"MORE!\\""',
-            '"MOARRRR!\\\\$\\\\$\\\\$!"'
+            `"${slash}"MORE!${slash}""`,
+            `"MOARRRR!${doubleSlash}$${doubleSlash}$${doubleSlash}$!"`
         ].join(',\n');
 
         const actualText = transformToBodyText(rawText);
