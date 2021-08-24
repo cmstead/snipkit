9925b2662df1722990cf3253f51d202502dc08f4
Chris Stead <cmstead@gmail.com>
2021-08-24 11:29:13 -0700

    F Getting basic tab stop offset data

--- modules/tab-stop-service.js
+++ modules/tab-stop-service.js
@@ -1,5 +1,31 @@
+function reverseString(stringToReverse) {
+    let reversedBodyString = stringToReverse.split('');
+    reversedBodyString.reverse();
+    return reversedBodyString.join('');
+}
+
 function getOffset(snippetBodyString, cursorLocation) {
-    return { start: 3, end: 0 };
+    const reversedBodyString = reverseString(snippetBodyString);
+    let currentToken = '';
+    let index = 0;
+
+    for (let i = reversedBodyString.length - (cursorLocation + 1); i < reversedBodyString.length; i++) {
+        const character = reversedBodyString[i];
+
+        currentToken += character;
+
+        if (!/^[0-9]+\{?\$?$/.test(currentToken)) {
+            currentToken = '';
+        } else if (/[0-9]+\{?\$/.test(currentToken)) {
+            index = i;
+            break;
+        }
+    }
+
+    const offset = cursorLocation - currentToken.length + 1;
+    const tokenStart = offset + currentToken.length;
+    
+    return { start: tokenStart, end: offset };
 }
 
 module.exports = {
--- test/tab-stop-service.test.js
+++ test/tab-stop-service.test.js
@@ -4,7 +4,7 @@ const { getOffset } = require('../modules/tab-stop-service');
 
 describe('tab stop service', function () {
     describe('get tab stop start offset', function () {
-        it('gets the tab stop start offset information when cursor is at the end of the tab stop', function () {
+        it('gets the tab stop start offset information when cursor is at the end of the tab stop number', function () {
             const snippetBodyString = '${2:Something}';
             const cursorLocation = 2;
 
@@ -13,5 +13,15 @@ describe('tab stop service', function () {
 
             assert.deepEqual(offset, expectedOffset);
         });
-    })
+
+        it('gets the tab stop start offset information when cursor is inside tab stop, not at end of tab stop number', function () {
+            const snippetBodyString = '${2:Something}';
+            const cursorLocation = 4;
+
+            const offset = getOffset(snippetBodyString, cursorLocation);
+            const expectedOffset = { start: 5, end: 2 };
+
+            assert.deepEqual(offset, expectedOffset);
+        });
+    });
 });
